aa166100765ef732283952404359fcde500e5190
femamanic <femamanic@unjbg.edu.pe>
2024-08-06 17:08:03 -0500

    Agregar GerenciaComercial.h: archivo .h donde se define las funciones del Gerente de Tienda.

---
+++ GerenciaComercial/GerenciaComercial.h
@@ -0,0 +1,11 @@
+#ifndef GERENCIACOMERCIAL_H
+#define GERENCIACOMERCIAL_H
+
+void menuGerenteDeTienda();
+void comprarProducto();
+void agregarProducto();
+void eliminarProducto();
+void mostrarRegistroDeInventario();
+void modificarProducto();
+
+#endif // !GERENCIACOMERCIAL_H
