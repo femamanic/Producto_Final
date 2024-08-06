594b5c62e589d1a7d7767f5cbf73ac749bd6f402
femamanic <femamanic@unjbg.edu.pe>
2024-08-06 17:07:11 -0500

    Agregar AdministracionYFinanzas.h: archivo .h donde se define las funciones de contabilidad, RRHH y tesoreria.

---
+++ GerenciaDeAdministracionYFinanzas/AdministracionYFinanzas.h
@@ -0,0 +1,18 @@
+#ifndef ADMINISTRACIONYFINANZAS_H
+#define ADMINISTRACIONYFINANZAS_H
+
+void menuContabilidad();
+void verReporteDeVentas();
+void verReporteDeCompras();
+
+void menuTesoreria();
+void pagarSueldos();
+void pagarProveedores();
+
+void menuRRHH();
+void contratarPersonal();
+void despedirPersonal();
+void verRegistroDePersonal();
+
+
+#endif // !ADMINISTRACIONYFINANZAS_H
