84f37df14b73397493e1e9cef24f21cb3c87fec7
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-08-09 02:32:02 -0500

    se culmino la funcion pero aun esta sometida a distintos cambios

--- CajaRegistradora/buscarProducto.cpp
+++ CajaRegistradora/buscarProducto.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void buscarProducto () {
     string m;
-    int n,c=0;
+    int n,c=0,h;
     do{
      system ("cls");
     cout << "+---------------------------------+\n";
@@ -23,15 +23,57 @@ void buscarProducto () {
     {
     case 1:
         cin.ignore( );
-        cout<<"ingrese el nombre del rpoducto a buscar: ";
+        cout<<"ingrese el nombre del rpoducto a buscar: ";getline(cin, m);
+        for(int i=0;i<numeroProductos;i++){
+            if(producto[i].nombre==m){
+                if(producto[i].unidadesEnAlmacen<=0){
+                    cout<<"el producto que acaba de buscar ya no hay en el almacen."<<endl;
+                }
+                cout<<"el numero de unidades disponibles es: "<<producto[i].unidadesEnAlmacen<<endl;
+            }
+        }
         break;
     case 2:
+        cin.ignore( );
+        cout<<"ingrese la categoria del producto: ";getline(cin, m);
+        for(int i=0;i<numeroProductos;i++){
+            if(producto[i].categoria==m){
+                cout<<"el nombre del producto es: "<<producto[i].nombre<<endl;
+                if(producto[i].unidadesEnAlmacen<=0){
+                    cout<<"el producto que acaba de buscar ya no hay en el almacen."<<endl;
+                }
+                cout<<"el numero de unidades disponibles es: "<<producto[i].unidadesEnAlmacen<<endl;
+            }
+        }
         break;
     case 3:
+        cout<<"ingrese la marca del producto: ";getline(cin,m);
+        for(int i=0;i<numeroProductos;i++){
+            if(producto[i].marca==m){
+                cout<<"el nombre del producto es: "<<producto[i].nombre<<endl;
+                if(producto[i].unidadesEnAlmacen<=0){
+                    cout<<"el producto que usted buscar ya no hay en el almacen."<<endl;
+                }else{
+                    cout<<"El numero de unidades disponibles es: "<<producto[i].unidadesEnAlmacen<<endl;
+                }
+            }
+        }
         break;
     case 4:
+        cout<<"ingrese el precio del producto: ";cin>>h;
+        for(int i=0;i<numeroProductos;i++){
+            if(producto[i].precio==h){
+                cout<<"el nombre del producto es: "<<producto[i].nombre<<endl;
+                if(producto[i].unidadesEnAlmacen<=0){
+                    cout<<"el producto que acaba de buscar se vendio todo."<<endl;
+                }else{
+                cout<<"el numero de unidades diponibles es: "<<producto[i].unidadesEnAlmacen<<endl;
+                }
+            }
+        }
         break;
     case 0:
+        cout<<"REGRESANDO..."<<endl;
         break;
     default:
         cout<<"ERROR: OPCION INVALIDA"<<endl;
