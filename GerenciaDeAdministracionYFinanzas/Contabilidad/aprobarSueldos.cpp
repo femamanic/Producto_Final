6a629c27826bf99a24f82de33e6e1fbb08a54de8
femamanic <femamanic@unjbg.edu.pe>
2024-08-09 12:38:44 -0500

    Corregir archivo: solo aprobar sueldos de los que falta

--- GerenciaDeAdministracionYFinanzas/Contabilidad/aprobarSueldos.cpp
+++ GerenciaDeAdministracionYFinanzas/Contabilidad/aprobarSueldos.cpp
@@ -11,23 +11,25 @@ void aprobarSueldos() {
     cout << "|                 Aprobar Sueldos                           |" << endl;
     cout << "+-----------------------------------------------------------+" << endl;
     for (int i = 0; i < numeroEmpleados; i++) {
-        cout << "[" << i+1 << "] Codigo: " << empleado[i].codigo;
-        cout << "\n Nombre: " << empleado[i].nombre << endl;
-        cout << " Cargo: " << empleado[i].cargo << endl;
-        cout << " Sueldo: S/." << empleado[i].salario << endl;
-        cout << "+-----------------------------------------------------------+" << endl;
-        cout << " Aprobar sueldo? : \n";
-        cout << " [1] Si \n [2] No \n [0] Salir \n";
-        cout << "+-----------------------------------------------------------+" << endl;
-        cout << " Opcion: "; cin >> opcion;
-        if (opcion == 1) {
-            empleado[i].sueldoAprobado = true;
-        } else if (opcion == 2) {
-            empleado[i].sueldoAprobado = false;
-        } else if (opcion == 0) {
-            break;
-        } else {
-            cout << " Opcion invalida\n";
+        if (empleado[i].sueldoAprobado == false) {
+            cout << "[" << i+1 << "] Codigo: " << empleado[i].codigo;
+            cout << "\n Nombre: " << empleado[i].nombre << endl;
+            cout << " Cargo: " << empleado[i].cargo << endl;
+            cout << " Sueldo: S/." << empleado[i].salario << endl;
+            cout << "+-----------------------------------------------------------+" << endl;
+            cout << " Aprobar sueldo? : \n";
+            cout << " [1] Si \n [2] No \n [0] Salir \n";
+            cout << "+-----------------------------------------------------------+" << endl;
+            cout << " Opcion: "; cin >> opcion;
+            if (opcion == 1) {
+                empleado[i].sueldoAprobado = true;
+            } else if (opcion == 2) {
+                empleado[i].sueldoAprobado = false;
+            } else if (opcion == 0) {
+                break;
+            } else {
+                cout << " Opcion invalida\n";
+            }
         }
     } 
     system ("pause>nul");
