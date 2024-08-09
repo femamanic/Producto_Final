272baa7dc5317bf19de4aaeb559a7fadbb511545
femamanic <femamanic@unjbg.edu.pe>
2024-08-08 23:46:07 -0500

    Actualizar variables.cpp

--- variables.cpp
+++ variables.cpp
@@ -5,3 +5,20 @@ int numeroEmpleados = 0;
 
 inventario producto[100];
 int numeroProductos = 0;
+
+string diaActual[7] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+string semanaActual[4] = {"Semana1", "Semana2", "Semana3", "Semana4"};
+string mesActual[12] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
+int diaAct = 0;
+int semanaAct = 0;
+int mesAct = 0;
+int anioAct = 0;
+
+float ventasMensuales[7][4] = {0};
+float conprasMensuales[7][4] = {0};
+float gastosMensuales[7][4] = {0};
+
+float ventasAnuales[12][50] = {0};
+float conprasAnuales[12][50] = {0};
+float gastosAnuales[12][50] = {0};
