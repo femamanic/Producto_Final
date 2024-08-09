0ed76b91fd98b51d0e791e4a7ac09c914f9f8b46
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-08-09 01:52:13 -0500

    se agrego el menu de opciones y tambien un contador de intentos erroneos

--- CajaRegistradora/buscarProducto.cpp
+++ CajaRegistradora/buscarProducto.cpp
@@ -6,8 +6,42 @@ using namespace std;
 
 void buscarProducto () {
     string m;
+    int n,c=0;
+    do{
+     system ("cls");
     cout << "+---------------------------------+\n";
     cout << "|          Buscar Producto        |\n";
     cout << "+---------------------------------+\n";
-    
+    cout << "| [1] Por nombre                  |\n";
+    cout << "| [2] Por categoria               |\n";
+    cout << "| [3] Por marca                   |\n";
+    cout << "| [4] Por precio                  |\n";
+    cout << "| [0] Regresar                    |\n";
+    cout << "+---------------------------------+\n";
+    cout << "elija una opcion: ";cin>>n;
+    switch (n)
+    {
+    case 1:
+        cin.ignore( );
+        cout<<"ingrese el nombre del rpoducto a buscar: ";
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 0:
+        break;
+    default:
+        cout<<"ERROR: OPCION INVALIDA"<<endl;
+        cout<<"intente otra vez."<<endl;
+        c++;
+        break;
+    }
+    if(n>3){
+        cout<<"unted ha superado la maxima cantidad de intentos erroneos."<<endl;
+        n=0;
+    }
+    }while(n!=0);
 }
