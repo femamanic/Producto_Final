be481bfd89166f67716bceda63954fd60fb91176
imaquerac <imaquerac@unjbg.edu.pe>
2024-08-09 00:40:48 -0500

    Agregar Comprar Producto: Se actualiza los cambios

--- GerenciaComercial/comprarProducto.cpp
+++ GerenciaComercial/comprarProducto.cpp
@@ -4,11 +4,14 @@
 using namespace std;
 
 void ComprarProducto() {
+    cout << "+-----------------------------------+" << endl;
+    cout << "|         Comprar Productos         |" << endl;
+    cout << "+-----------------------------------+" << endl;
     int codigo, cantidad;
-    cout << "Ingrese el cÃ³digo del producto que desea comprar: ";cin >> codigo;
+    cout << "Ingrese el codigo del producto que desea comprar: ";cin >> codigo;
     cout << "Ingrese la cantidad que desea comprar: ";cin >> cantidad;
 
-    for (int i = 0; i < nProductos; i++) {
+    for (int i = 0; i < numeroProductos; i++) {
         if (producto[i].codigo == codigo) {
             if (producto[i].cantidad >= cantidad) {
                 producto[i].cantidad -= cantidad;
