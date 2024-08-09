466a974c3b53605843290e8555022aaa82276806
femamanic <femamanic@unjbg.edu.pe>
2024-08-09 12:29:34 -0500

    Actualizar variables

--- variables.cpp
+++ variables.cpp
@@ -10,10 +10,6 @@ compra carrito[100];
 int numerocarrito = 0;
 float totalCarrito = 0;
 
-string diaActual[7] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
-string semanaActual[4] = {"Semana1", "Semana2", "Semana3", "Semana4"};
-string mesActual[12] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
-
 float compraProveedores = 0;
 float gastoSueldos = 0;
 float caja = 0;
