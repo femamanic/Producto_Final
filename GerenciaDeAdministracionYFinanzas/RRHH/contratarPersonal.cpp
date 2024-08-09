439dcd94c03188c54f784dce41e118dd0b274ddc
hugo-quispe <hordonoq@unjbg.edu.pe>
2024-08-08 22:46:44 -0500

    agregar distintos mensajes para el usuario el cual es que ingrese el nombre de su nuevo personal

--- GerenciaDeAdministracionYFinanzas/RRHH/contratarPersonal.cpp
+++ GerenciaDeAdministracionYFinanzas/RRHH/contratarPersonal.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "../AdministracionYFinanzas.h"
+#include "../../variables.h"
 using namespace std;
 void contratarPersonal(){
-    
+    int n;
+    cout << "+---------------------------------+\n";
+    cout << "|        Contratar Personal       |\n";
+    cout << "+---------------------------------+\n";
+    cout<<"Elija cuantas personas desea contratar: ";cin>>n;
+    for(int i=0;i<n;i++){
+        numeroEmpleados=numeroEmpleados+1;
+        cin.ignore( );
+        cout<<"Ingrese el Nombre y Apellido: ";getline(cin,empleado[numeroEmpleados].nombre);
+    }
 }
