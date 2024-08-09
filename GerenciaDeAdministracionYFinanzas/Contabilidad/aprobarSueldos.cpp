928b06f494ef54151ed471ae3e830325b1a30a6f
femamanic <femamanic@unjbg.edu.pe>
2024-08-09 12:36:39 -0500

    Se agrego la opcion de salir directamente sin la necesidad de aprobar todos los sueldo.

--- GerenciaDeAdministracionYFinanzas/Contabilidad/aprobarSueldos.cpp
+++ GerenciaDeAdministracionYFinanzas/Contabilidad/aprobarSueldos.cpp
@@ -16,12 +16,18 @@ void aprobarSueldos() {
         cout << " Cargo: " << empleado[i].cargo << endl;
         cout << " Sueldo: S/." << empleado[i].salario << endl;
         cout << "+-----------------------------------------------------------+" << endl;
-        cout << " ¿Aprobar sueldo? : \n";
-        cout << " [1] Si \n [2] No \n";
+        cout << " Aprobar sueldo? : \n";
+        cout << " [1] Si \n [2] No \n [0] Salir \n";
         cout << "+-----------------------------------------------------------+" << endl;
         cout << " Opcion: "; cin >> opcion;
         if (opcion == 1) {
             empleado[i].sueldoAprobado = true;
+        } else if (opcion == 2) {
+            empleado[i].sueldoAprobado = false;
+        } else if (opcion == 0) {
+            break;
+        } else {
+            cout << " Opcion invalida\n";
         }
     } 
     system ("pause>nul");
